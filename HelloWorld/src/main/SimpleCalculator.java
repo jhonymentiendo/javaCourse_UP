1b4c045d19e365ca8e4fa5f5042c338981a7a890
337289 <jiglesias@elektra.com.mx>
2022-12-05 18:36:07 -0600

    cambio calculadora

--- HelloWorld/src/main/SimpleCalculator.java
+++ HelloWorld/src/main/SimpleCalculator.java
@@ -2,4 +2,41 @@ package main;
 
 public class SimpleCalculator {
 
+	double firstNumber; 
+	double secondNumber;
+	
+	
+	
+	public double getAdditionResult() {
+	return this.getFirstNumber() + this.getSecondNumber();
+}
+	
+	public double getSubtractionResult() {
+		return this.getFirstNumber() - this.getSecondNumber();
+	}
+	
+	public double getMultiplicationResult() {
+		return this.getFirstNumber() * this.getSecondNumber();
+	}
+	
+	public double getDivisionResult() {
+		return this.getSecondNumber()==0 ? 0.0 : this.getFirstNumber() / this.getSecondNumber();
+	}
+	
+	public double getFirstNumber() {
+		return firstNumber;
+	}
+	public double getSecondNumber() {
+		return secondNumber;
+	}
+	public void setFirstNumber(double firstNumber) {
+		this.firstNumber = firstNumber;
+	}
+	public void setSecondNumber(double secondNumber) {
+		this.secondNumber = secondNumber;
+	}
+	
+	
+	
+	
 }
